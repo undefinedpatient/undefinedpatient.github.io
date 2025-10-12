f0684d2894f3224ce26b315784d6d1e8778354e2
patient <undefinedpatient@gmail.com>
2025-10-12 15:54:43 +0800

    Trying all out

---
+++ out/_next/static/chunks/48c34874ea28dbfc.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/f9f6f979f399ede6.js",
+  "static/chunks/turbopack-cbe3c998daee3ccd.js"
+])
---
+++ out/_next/static/chunks/9101051d0976968f.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/f9f6f979f399ede6.js",
+  "static/chunks/turbopack-49761a69f80016cf.js"
+])
