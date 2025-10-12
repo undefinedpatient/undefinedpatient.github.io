913e35064c27d6d74d72fd50c661827235bf937e
patient <undefinedpatient@gmail.com>
2025-10-12 16:04:00 +0800

    Added font subset

--- src/app/ui/fonts.ts
+++ src/app/ui/fonts.ts
@@ -1,7 +1,7 @@
 import { Inter, Fira_Code, Playfair_Display, Roboto_Slab, Doto } from "next/font/google";
 
-export const inter = Inter();
-export const fira_code = Fira_Code();
-export const playfair_display = Playfair_Display();
-export const roboto_slab = Roboto_Slab();
-export const doto = Doto();
+export const inter = Inter({ subsets: ["latin"] });
+export const fira_code = Fira_Code({ subsets: ["latin"] });
+export const playfair_display = Playfair_Display({ subsets: ["latin"] });
+export const roboto_slab = Roboto_Slab({ subsets: ["latin"] });
+export const doto = Doto({ subsets: ["latin"] });
