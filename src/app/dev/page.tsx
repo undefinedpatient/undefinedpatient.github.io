a8bffde7d2852c3f1df2084ed7d9ae12e4e7a69f
undefinedpatient <undefinedpatient@gmail.com>
2025-11-26 22:59:45 +0800

    Updated info

--- src/app/dev/page.tsx
+++ src/app/dev/page.tsx
@@ -53,14 +53,16 @@ function DevPage() {
 
                 </div>
                 <div>
-                    <CardTable title="Programming Languages" subtitle="I am narrowing it down">
-                        <ProgressionCard title="[C++]" description="c++26" progression={0.4} proportions={8} />
-                        <ProgressionCard title="[Javascript]" description="Weak type!" progression={0.6} proportions={8} />
-                        <ProgressionCard title="[Scala3]" description="LPLPLPLPLPLP" progression={0.5} proportions={8} />
-                        <ProgressionCard title="[C]" description="pthread" progression={0.2} proportions={8} />
-                        <ProgressionCard title="Typescript" description="Strong type?" progression={0.3} proportions={8} />
+                    <CardTable title="Programming Languages (Active)" subtitle="I begin to know what I need to stick with.">
+                        <ProgressionCard title="C++" description="c++26" progression={0.6} proportions={8} />
+                        <ProgressionCard title="Javascript" description="Weird" progression={0.4} proportions={8} />
+                    </CardTable>
+                    <CardTable title="Programming Languages (Inactive)" subtitle="Simple, the languages I am currently not using/leaving because I no longer get a chance to use!">
+                        <ProgressionCard title="Scala3" description="LP" progression={0.6} proportions={8} />
+                        <ProgressionCard title="C" description="pthread" progression={0.2} proportions={8} />
+                        <ProgressionCard title="Typescript" description="Strong type?" progression={0.1} proportions={8} />
                         <ProgressionCard title="Java" description="Rarely used" progression={0.5} proportions={8} />
-                        <ProgressionCard title="Dart" description="Refer to my project" progression={0.7} proportions={8} />
+                        <ProgressionCard title="Dart" description="Refer to my project" progression={0.5} proportions={8} />
                         <ProgressionCard title="Python" description="Course focused language" progression={0.1} proportions={8} />
                     </CardTable>
                 </div>
