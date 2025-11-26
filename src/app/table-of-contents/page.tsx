8fb1df2595deeefe890daa7a4ed8bd77e109ec65
undefinedpatient <undefinedpatient@gmail.com>
2025-11-26 21:34:22 +0800

    Updated info

--- src/app/table-of-contents/page.tsx
+++ src/app/table-of-contents/page.tsx
@@ -16,7 +16,7 @@ function TableOfContentsPage() {
                     <hr className="hr-animated-lr" />
                     &emsp;Patient (Ethan, Choi), born in 2005, discovered early on the joy of creation and the transformative power of computers. Inspired by the possibilities they unlock, he began his journey into the worlds of art and computer science at the age of 18.
                     <br />
-                    &emsp;With a keen eye for aesthetics, Ethan is passionate about frontend development, placing strong emphasis on visual detail and user experience. He is currently exploring the Next.js framework, which he believes will be instrumental in shaping his future career.
+                    &emsp;With a keen eye for aesthetics, Ethan is passionate about development with strong emphasis on visual detail and user experience. He is currently exploring the Next.js framework, which he believes will be instrumental in shaping his future career.
                     <br />
                     &emsp;Ethan is currently a junior pursuing a Computer Science degree at&nbsp;
                     <a href="https://hkust.edu.hk/" className="underline">
